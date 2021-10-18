c7099c21515861f00867321411cd448809edb1dc
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2021-10-18 20:46:25 +0530

    NaN condition is added incase of empty list

--- statistics.py
+++ statistics.py
@@ -1,7 +1,12 @@
 
 def calculateStats(numbers):
   computedStats = {}
-  computedStats["avg"] = sum(numbers)/len(numbers)
-  computedStats["max"] = max(numbers)
-  computedStats["min"] = min(numbers)
+  if numbers==[]:
+    computedStats["avg"] = math.nan
+    computedStats["max"] = math.nan
+    computedStats["min"] = math.nan
+  else:
+    computedStats["avg"] = sum(numbers)/len(numbers)
+    computedStats["max"] = max(numbers)
+    computedStats["min"] = min(numbers)
   return computedStats
