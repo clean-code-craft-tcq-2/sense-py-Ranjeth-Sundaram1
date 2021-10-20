80e781ec5761daa273ddb67b2f009bc5952cdfe9
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2021-10-20 21:02:52 +0530

    new class for StatsAlerter is added

---
+++ StatsAlerter.py
@@ -0,0 +1,6 @@
+class StatsAlerter():
+  def __init__(self, maxThreshold, [emailAlert, ledAlert]):
+    self.maxThreshold = maxThreshold
+    self.emailAlert = emailAlert
+    selft.ledAlert = ledAlert
+    
