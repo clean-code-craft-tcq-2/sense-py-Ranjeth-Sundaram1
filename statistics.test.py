df487d4c2e91058baf9c728df0ce21fa6330fa34
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2021-10-23 00:09:55 +0530

    new classes added

--- statistics.test.py
+++ statistics.test.py
@@ -1,9 +1,48 @@
 import unittest
 import statistics
 import math
-from StatsAlerter import EmailAlert,LEDAlert
 import StatsAlerter
 
+class LEDAlert():
+  def __init__(self):
+    self.AlertValue = 1
+  def make_led_on():
+    print('led is turned ON')
+    
+class EmailAlert():
+  def __init__(self):
+    self.sendto = "ranjethsudhakar@gmail.com" 
+    self.sendfrom = "ranjethsundaram@gmail.com"
+    self.password = 'in.bosch.com11'
+    
+  def construct_message():
+    message = '''
+          <html>
+            <body>
+                <p>Hi,<br/><br/>
+                   The sensor values exceeds the maximum threshold value. Please take necessary actions to avoid problems.
+                <br/>
+                Thank you.<br/>
+                <p>******************* This is an auto generated email. Please do not reply to this mail *******************</p>
+                <br/>
+            <p>Regards,<br/>statsAlerterTeam</p>
+            </body>
+        </html>
+        '''
+    return message
+  
+    def send_mail():
+      mail_content = construct_message()
+      subject = "Alert: values exceeds"
+      port = 587  
+      smtp_server = "smtp.gmail.com"
+      context = ssl.create_default_context()
+      with smtplib.SMTP(smtp_server, port) as server:
+          server.ehlo()
+          server.starttls(context=context)
+          server.ehlo()
+          server.login(self.sendfrom, self.password)
+          server.sendmail(self.sendfrom, self.sendto, mail_content)
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
